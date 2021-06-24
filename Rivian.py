ff3250b90f54e5ae48ddf7b83115e970975e85c8
Paul Yang <yangfuy@gmail.com>
2021-06-23 23:17:19 -0700

    Rivian.py

--- Rivian.py
+++ Rivian.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import math
-#This is a test
+
 
 def Check_Stream_Time(S):
     dict = {}
@@ -21,46 +21,6 @@ def Check_Stream_Time(S):
     return dict
 
 
-def Count_cost(data):
-    cost = {}
-
-    discount = ''
-    for each_stream in data:
-        total = 0
-        for each_time in data[each_stream]:
-            for timestamp in each_time.keys():
-
-               # print (each_time[timestamp])
-                if each_time[timestamp] <180:
-                    each_time[timestamp] = math.ceil(4* float(each_time[timestamp]))
-
-                elif each_time[timestamp] >= 180:
-                    each_time[timestamp] = math.ceil(float(each_time[timestamp] / 60.00))* 225
-            total += each_time[timestamp]
-
-        data[each_stream].append(total)
-
-        if discount =='':
-            max_stream = each_stream
-            max = data[each_stream][-1]
-            max_stream_num = int(''.join(max_stream.split('-')))
-
-        stream_num = int(''.join(each_stream.split('-')))
-        if data[each_stream][-1] >= max:
-            if stream_num >= max_stream_num:
-                discount = each_stream
-
-    for each_time in data[discount]:
-      if type(each_time) is dict:
-          for timestamp in each_time.keys():
-              each_time[timestamp]= each_time[timestamp]//2
-      else:
-          data[discount][-1] = each_time//2
-
-    print ("50% discount awarded to stream: {}\n".format(discount))
-    print ("Cost is calculated to associated stream:\n{}".format(data))
-
-    return data,discount
 
 def print_out(data,discount):
     print ("\nDetail BreakDown")
