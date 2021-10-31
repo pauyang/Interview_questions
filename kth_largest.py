fe8749c6aa72c3be0cc3c70e57ff6e7f860cf649
Paul Yang <yangfuy@gmail.com>
2021-10-31 14:56:10 -0700

    leetcode

--- kth_largest.py
+++ kth_largest.py
@@ -1,3 +1,41 @@
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest(self, nums, k) :
+        new_nums = self.quck_sort(nums)
+        return new_nums[-k]
+
+
+
+
+    def quck_sort(self,nums):
+
+
+
+        if len(nums)> 1:
+            pivot = nums.pop()
+            less = []
+            more = []
+            equal = [pivot]
+
+
+
+            for i in nums:
+                if i > pivot:
+                    more.append(i)
+                elif i < pivot:
+                    less.append(i)
+                else:
+                    equal.append(i)
+
+            return self.quck_sort(less)+equal+self.quck_sort(more)
+
+        else:
+            return nums
+
+
+nums = [3,2,1,5,6,4]
+k = 2
+
+s= Solution()
+t=s.findKthLargest(nums,k)
+print (t)
--- test.py
+++ test.py
@@ -1,10 +1,49 @@
-def window(iterable, size=2):
-    i = iter(iterable)
-    win = []
-    for e in range(0, size):
-        win.append(next(i))
-    yield win
-    for e in i:
-        win = win[1:] + [e]
-        yield win
+def selection_sort (array):
+    mi = 0
+    for i in range(len(array)):
+        for j in range(mi+1,len(array)):
+            if array[j]<array[mi]:
+                array[j],array[mi]=array[mi],array[j]
+        mi+=1
 
+    return array
+
+def bubble_sort(array):
+    for i in range(len(array)):
+        for j in range(len(array)-1):
+            if array[j] >array[j+1]:
+                array[j+1],array[j]= array[j],array[j+1]
+
+
+    return array
+
+def quicksort(array):
+
+    if len(array)>1:
+        pivot = array.pop()
+        less =[]
+        more =[]
+        equal =[pivot]
+
+        for i in range(len(array)):
+            if array[i]<pivot:
+                less.append(array[i])
+            elif array[i]>pivot:
+                more.append(array[i])
+            else:
+                equal.append(array[i])
+
+        return quicksort(less)+equal+quicksort(more)
+
+    else:
+        return array
+
+
+
+
+
+array = [4,5,3,1,7,8,2]
+#t=selection_sort(array)
+#t= bubble_sort(array)
+t=quicksort(array)
+print(t)
